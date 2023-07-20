dea3f80a50e6fc1df67074ec6009c77f9785e068
hamitmizrak <hamitmizrak@gmail.com>
2023-07-20 19:47:17 +0300

    Project Template second

--- src/main/java/com/hamitmizrak/KodlamaAtolyesi1Application.java
+++ src/main/java/com/hamitmizrak/KodlamaAtolyesi1Application.java
@@ -1,13 +1,66 @@
 package com.hamitmizrak;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+import java.util.TimeZone;
+
+
+// Mongo aktif etmek ici
+// @EnableMongoRepositories
+
+// Aspect aktif etmek icin
+// @EnableAspectJAutoProxy(proxyTargetClass = true)
+
+// Asenkron açmak icin
+// @EnableAsync
+
+// SCAN
+// @EntityScan(basePackages = "com.hamitmizrak.data.entity") //Entity bulamadığı zaman
+// @EnableJpaRepositories(basePackages = "com.hamitmizrak.data.repository") //Repository bulamadığı zaman
+
+// Spring Cache aktif etmek gerekiyor
+// @EnableCaching
+
+// auditorAware icin
+//@EnableJpaAuditing(auditorAwareRef = "auditorAwareMethod")
+
+// Spring Security
+@SpringBootApplication(exclude = {
+        //SecurityAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+}
+)
+/*@SpringBootApplication*/
 public class KodlamaAtolyesi1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KodlamaAtolyesi1Application.class, args);
-	}
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("IST"));
+    }
+
+    public static void main(String[] args) {
+
+
+        // devtools active isActive
+        //System.setProperty("spring.devtools.restart.enabled","true");
+
+        // PORT Ayarlamak
+        /*
+        SpringApplication app = new SpringApplication(TurgutUniversitySpringAllInOneApplication.class);
+        app.setDefaultProperties(Collections
+                .singletonMap("server.port", "8083"));
+        app.run(args);
+         */
+
+        //Disables headless JOptionPane
+        System.setProperty("java.awt.headless", "false");
+
+        SpringApplication.run(KodlamaAtolyesi1Application.class, args);
+    }
 
 }
--- src/main/java/com/hamitmizrak/util/Persist.java
+++ src/main/java/com/hamitmizrak/util/Persist.java
@@ -1,2 +1,8 @@
-package com.hamitmizrak.util;public class Persist {
+package com.hamitmizrak.util;
+
+public class Persist {
+    // Persist URL  : http://localhost:4444/
+    // DATABASE URL : http://localhost:4444/h2-console
+    // API URL      : http://localhost:4444/swagger-ui/index.html
+    // GİTHUB URL   : https://github.com/hamitmizrak/kodlama_atolyesi_1
 }
